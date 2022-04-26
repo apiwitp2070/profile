4548282e6964e6fe6bdfb58d5f56016fc8f6c890
apiwit2070 <zenzoza007@gmail.com>
2022-04-26 21:02:39 +0700

    add myinfo file

---
+++ public/js/data/myinfo.js
@@ -0,0 +1,5 @@
+export const myname =
+  "Hello, My name is Apiwit prasittikarnkul. You can call me Yoseph or Yo."
+
+export const myinfo = 
+  "Graduated from Kasetsart University, with a Bachelor degree in Computer Engineering. I am junior frontend developer who have interest in using and improving my skill to develop a simple yet beautiful website in a various way."
